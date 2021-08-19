74758174b6f602dc9fffc4eec875b4c85d6e7ab2
Victor Franco <vfrancohdez1@hotmail.com>
2021-08-19 18:07:58 -0500

    Add exit option in the menu
    Insert the menu into a loop, when the user select exit option break the
    loop.

--- src/main.rs
+++ src/main.rs
@@ -5,28 +5,32 @@ mod crud;
 fn main() {
     let mut clients:HashMap<String,crud::Client> = HashMap::new();
 
-    std::process::Command::new("clear").status().unwrap();// clear screen
-    println!("-------------------------------\n\
-              |            Menu             |\n\
-              -------------------------------\n\
-              |                             |\n\
-              |    1. Agregar Cliente       |\n\
-              |    2. Modificar Cliente     |\n\
-              |    3. Eliminar Cliente      |\n\
-              |    4. Lista Clientes        |\n\
-              |                             |\n\
-              -------------------------------\n");
-    print!(">>> ");
-    let _ = io::stdout().flush();
-    let mut option = String::new();
-    io::stdin().read_line(&mut option).unwrap();//        get user input
-    println!("");
-    let option = option.trim().parse::<i32>().unwrap();// cast option
-    match option {
-        1 => crud::insert_client(&mut clients),
-        2 => crud::update_client(&mut clients),
-        3 => crud::delete_client(&mut clients),
-        4 => crud::show_clients(&mut clients),
-        _ => println!("Esa no es una opcion valida")
+    loop {
+        std::process::Command::new("clear").status().unwrap();// clear screen
+        println!("-------------------------------\n\
+                  |            Menu             |\n\
+                  -------------------------------\n\
+                  |                             |\n\
+                  |    1. Agregar Cliente       |\n\
+                  |    2. Modificar Cliente     |\n\
+                  |    3. Eliminar Cliente      |\n\
+                  |    4. Lista Clientes        |\n\
+                  |    5. Salir                 |\n\
+                  |                             |\n\
+                  -------------------------------\n");
+        print!(">>> ");
+        let _ = io::stdout().flush();
+        let mut option = String::new();
+        io::stdin().read_line(&mut option).unwrap();//        get user input
+        println!("");
+        let option = option.trim().parse::<i32>().unwrap();// cast option
+        match option {
+            1 => crud::insert_client(&mut clients),
+            2 => crud::update_client(&mut clients),
+            3 => crud::delete_client(&mut clients),
+            4 => crud::show_clients(&mut clients),
+            5 => break,
+            _ => println!("Esa no es una opcion valida")
+        }
     }
 }
