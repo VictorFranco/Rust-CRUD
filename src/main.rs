637126cf6be31b7aad4e950e9bb62ee16b9a856d
Victor Franco <vfrancohdez1@hotmail.com>
2021-08-19 09:04:53 -0500

    Create menu

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    std::process::Command::new("clear").status().unwrap();
+    println!("-------------------------------\n\
+              |            Menu             |\n\
+              -------------------------------\n\
+              |                             |\n\
+              |    1. Agregar Cliente       |\n\
+              |    2. Modificar Cliente     |\n\
+              |    3. Eliminar Cliente      |\n\
+              |    4. Lista Clientes        |\n\
+              |                             |\n\
+              -------------------------------\n");
+    print!(">>> ");
 }
