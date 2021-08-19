4f78013a89416247381529b5f46221ac19b0923a
Victor Franco <vfrancohdez1@hotmail.com>
2021-08-19 10:18:13 -0500

    Create a function for each menu option

---
+++ src/crud.rs
@@ -0,0 +1,8 @@
+pub fn create_client(){
+}
+pub fn update_client(){
+}
+pub fn delete_client(){
+}
+pub fn show_clients(){
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use std::io::{self, Write};
+mod crud;
 
 fn main() {
-    std::process::Command::new("clear").status().unwrap();
+    std::process::Command::new("clear").status().unwrap();// clear screen
     println!("-------------------------------\n\
               |            Menu             |\n\
               -------------------------------\n\
@@ -15,7 +16,14 @@ fn main() {
     print!(">>> ");
     let _ = io::stdout().flush();
     let mut option = String::new();
-    io::stdin().read_line(&mut option).unwrap();
-    let option = option.trim().parse::<i32>().unwrap();
-    println!("{}",option);
+    io::stdin().read_line(&mut option).unwrap();//        get user input
+    println!("");
+    let option = option.trim().parse::<i32>().unwrap();// cast option
+    match option {
+        1 => crud::create_client(),
+        2 => crud::update_client(),
+        3 => crud::delete_client(),
+        4 => crud::show_clients(),
+        _ => println!("Esa no es una opcion valida")
+    }
 }
